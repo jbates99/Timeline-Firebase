b239fe18afe5c4a9c01dd71adfe3949633144197
Joshua Bates <batesjoshua50@gmail.com>
2016-06-02 21:42:43 -0600

    ImageController

---
+++ ImageController.swift
@@ -0,0 +1,23 @@
+//
+//  ImageController.swift
+//  Timeline
+//
+//  Created by Joshua Bates on 6/2/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class ImageController {
+    
+    static func uploadImage(image: UIImage, completion: (String) -> Void) {
+        
+    }
+    
+    static func imageForIdentifier(identifier: String, completion: (UIImage?) -> Void) {
+        
+    }
+    
+    
+}
