b844e4ee1f3b1c9dcedf6cbb70c1d08a8f5ead67
Joshua Bates <batesjoshua50@gmail.com>
2016-06-04 14:02:13 -0600

    set up login in view controller

--- Timeline/LoginSignupViewController.swift
+++ Timeline/LoginSignupViewController.swift
@@ -15,8 +15,8 @@ class LoginSignupViewController: UIViewController {
         case signup
         case login
     }
-    
     var mode: ViewMode = .signup
+    
     
     // MARK: IBOutlets
     
@@ -29,10 +29,24 @@ class LoginSignupViewController: UIViewController {
     @IBOutlet var passwordTextField: UITextField!
     @IBOutlet var bioTextField: UITextField!
     
+    var fieldsAreValid: Bool {
+        switch mode {
+        case .signup:
+            if !usernameTextField.text!.isEmpty && !emailTextField.text!.isEmpty && !passwordTextField.text!.isEmpty {
+                return true
+            } else { return false }
+            
+        case .login:
+            if !emailTextField.text!.isEmpty && !passwordTextField.text!.isEmpty {
+                return true
+            } else { return false }
+            
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
 
@@ -41,30 +55,61 @@ class LoginSignupViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func loginButtonPressed(sender: AnyObject) {
+        mode = .login
+        updateViewBasedOnMode()
+    }
+    @IBAction func signUpButtonPressed(sender: AnyObject) {
+        mode = .signup
+        updateViewBasedOnMode()
+    }
     func updateViewBasedOnMode(){
         switch mode {
         case .signup:
             signupStackView.hidden = false
             selectionStackView.hidden = true
-            actionButton.titleLabel?.text = "Sign Up"
+            actionButton.setTitle("Sign Up", forState: .Normal)
         case .login:
             signupStackView.hidden = false
             selectionStackView.hidden = true
-            actionButton.titleLabel?.text = "Log In"
+            actionButton.setTitle("Log in", forState: .Normal)
             usernameTextField.hidden = true
             bioTextField.hidden = true
             urlTextField.hidden = true
         }
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    
+    @IBAction func actionButtonTapped(sender: AnyObject) {
+        let alertController = UIAlertController(title: "Try Again", message: nil, preferredStyle: .Alert)
+        let okAction = UIAlertAction(title: "OK", style: .Cancel, handler: nil)
+        alertController.addAction(okAction)
+        
+        guard fieldsAreValid else { return }
+        switch mode {
+        case .login:
+            UserController.authenticateUser(emailTextField.text!, password: passwordTextField.text!, completion: { (user) -> Bool in
+                if user != nil {
+                    
+                    self.dismissViewControllerAnimated(true, completion: nil)
+                    return true
+                } else {
+                    self.presentViewController(alertController, animated: true, completion: nil)
+                    return false }
+            })
+        case .signup:
+            UserController.createUser(emailTextField.text!, username: usernameTextField.text!, password: passwordTextField.text!, bio: nil, completion: { (user) -> Bool in
+                if user != nil {
+                    
+                    self.dismissViewControllerAnimated(true, completion: nil)
+                    return true
+                } else {
+                    self.presentViewController(alertController, animated: true, completion: nil)
+                    return false }
+            })
+            
+        }
+        
     }
-    */
 
 }
