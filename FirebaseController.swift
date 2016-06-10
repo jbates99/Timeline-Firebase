05d3bd317e28bdee53956c5f420b1c37b97bf933
Joshua Bates <batesjoshua50@gmail.com>
2016-06-09 20:25:37 -0600

    Firebase Controller

--- FirebaseController.swift
+++ FirebaseController.swift
@@ -24,11 +24,47 @@ class FirebaseController {
         })
     }
     
-    
+    static func observeDataAtEndpoint(endpoint: String, completion: (data: AnyObject?) -> Void) {
+        let baseForEndpoint = FirebaseController.base.child(endpoint)
+        baseForEndpoint.observeEventType(.Value, withBlock: { snapshot in
+            if snapshot.value is NSNull {
+                completion(data: nil)
+            } else {
+                completion(data: snapshot.value)
+            }
+        })
+        
+    }
 }
 
 protocol FirebaseType {
     var identifier: String? { get set }
     var endpoint: String { get }
-    //var jsonValue:
+    var jsonValue: [String: AnyObject] { get }
+    
+    init?(json: [String : AnyObject], identifier: String)
+    
+    mutating func save()
+    func delete()
+}
+
+extension FirebaseType {
+    mutating func save() {
+        var endpointBase = FirebaseController.base
+        
+        if let identifier = self.identifier {
+            endpointBase = FirebaseController.base.child(endpoint).child(identifier)
+        } else {
+            endpointBase = FirebaseController.base.child(endpoint).childByAutoId()
+            self.identifier = endpointBase.key
+        }
+        endpointBase.updateChildValues(self.jsonValue)
+    }
+    
+    func delete() {
+        if let identifier = self.identifier {
+            let endpointBase = FirebaseController.base.child(endpoint).child(identifier)
+            endpointBase.removeValue()
+        }
+    }
 }
