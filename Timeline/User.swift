f7880bc09b22a7bbfa6e6e11f148fd9cfde03f6c
Joshua Bates <batesjoshua50@gmail.com>
2016-06-09 23:15:55 -0600

    User for firebase

--- Timeline/User.swift
+++ Timeline/User.swift
@@ -8,17 +8,44 @@
 
 import Foundation
 
-struct User: Equatable {
-    let username: String
-    var bio: String?
-    let url: String?
-    let identifier: String?
+struct User: Equatable, FirebaseType {
     
-    init(username: String, bio: String? = nil, url: String? = nil, identifier: String?) {
+    private let kUsername = "username"
+    private let kBio = "bio"
+    private let kURL = "url"
+    
+    var username = ""
+    var bio: String?
+    var url: String?
+    var identifier: String?
+    var endpoint: String {
+        return "users"
+    }
+    var jsonValue: [String : AnyObject] {
+        var json: [String : AnyObject] = [kUsername: username]
+        if let bio = bio {
+            json.updateValue(bio, forKey: kBio)
+        }
+        if let url = url {
+            json.updateValue(url, forKey: kURL)
+        }
+        return json
+    }
+    
+    init(username: String, uid: String, bio: String? = nil, url: String? = nil, identifier: String?) {
         self.username = username
-        self.identifier = identifier
+        self.identifier = uid
         self.bio = bio
         self.url = url
+    }
+    
+    init?(json: [String : AnyObject], identifier: String) {
+        guard let username = json[kUsername] as? String else { return nil }
+        
+        self.username = username
+        self.bio = json[kBio] as? String
+        self.url = json[kURL] as? String
+        self.identifier = identifier
     }
 }
 
