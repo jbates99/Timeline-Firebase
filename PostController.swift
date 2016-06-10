d3f9e5d7ad1d126ad16bc67a8e9da7a7d45687c5
Joshua Bates <batesjoshua50@gmail.com>
2016-06-10 00:07:24 -0600

    Firebase Post Controller

--- PostController.swift
+++ PostController.swift
@@ -11,48 +11,124 @@ import Foundation
 
 class PostController {
     
-    static func fetchTimelineForUser(user: User, completion: (post: Post) -> Bool) {
-        
+    static func fetchTimelineForUser(user: User, completion: (posts: [Post]?) -> Void) {
+        UserController.followedByUser(user) { (followed) in
+            var allPosts: [Post] = []
+            let dispatchGroup = dispatch_group_create()
+            dispatch_group_enter(dispatchGroup)
+            postsForUser(UserController.sharedController.currentUser!, completion: { (posts) -> Void in
+                if let posts = posts {
+                    allPosts += posts
+                }
+                dispatch_group_leave(dispatchGroup)
+            })
+            if let followed = followed {
+                for user in followed {
+                    dispatch_group_enter(dispatchGroup)
+                    postsForUser(user, completion: { (posts) in
+                        if let posts = posts {
+                            allPosts += posts
+                        }
+                        dispatch_group_leave(dispatchGroup)
+                    })
+                }
+            }
+            dispatch_group_notify(dispatchGroup, dispatch_get_main_queue(), { () -> Void in
+                let orderedPosts = orderPosts(allPosts)
+                completion(posts: orderedPosts)
+            })
+        }
     }
     
-    static func addPost(image: UIImage, caption: String?, completion: (Post?) -> Bool) {
-        
+    static func addPost(image: UIImage, caption: String?, completion: (success: Bool, post: Post?) -> Void) {
+        ImageController.uploadImage(image) { (identifier) -> Void in
+            if let identifier = identifier {
+                var post = Post(imageEndPoint: identifier, caption: caption, username: UserController.sharedController.currentUser!.username, identifier: identifier) //Why are there two identifiers
+                post.save()
+                completion(success: true, post: post)
+            } else {
+                completion(success: false, post: nil)
+            }
+        }
     }
     
-    static func postFromIdentifier(identifier: String, completion: (Post?) -> Void) {
-        
+    static func postFromIdentifier(identifier: String, completion: (post: Post?) -> Void) {
+        FirebaseController.dataAtEndpoint("posts/\(identifier)") { (data) -> Void in
+            if let data = data as? [String : AnyObject] {
+                let post = Post(json: data, identifier: identifier)
+                completion(post: post)
+            } else {
+                completion(post: nil)
+            }
+        }
     }
     
-    static func postsForUser(user: User, completion: ([Post]?) -> Void) {
-        
+    static func postsForUser(user: User, completion: (posts: [Post]?) -> Void) {
+        FirebaseController.base.child("posts").queryOrderedByChild("username").queryEqualToValue(user.username).observeSingleEventOfType(.Value, withBlock: { snapshot in
+            if let postDictionaries = snapshot.value as? [String : AnyObject] {
+                let posts = postDictionaries.flatMap({Post(json: $0.1 as! [String : AnyObject], identifier: $0.0)})
+                let orderedPosts = orderPosts(posts)
+                completion(posts: orderedPosts)
+            } else {
+                completion(posts: nil)
+            }
+        })
     }
     
     static func deletePost(post: Post) {
-        
+        post.delete()
     }
     
-    static func addCommentWithTextToPost(text: String, post: Post, completion: (Post?) -> Bool) {
-        
+    static func addCommentWithTextToPost(text: String, post: Post, completion: (success: Bool, post: Post?) -> Void?) {
+        if let postIdentifier = post.identifier {
+            var comment = Comment(username: UserController.sharedController.currentUser!.username, text: text, postIdentifier: postIdentifier)
+            comment.save()
+            PostController.postFromIdentifier(comment.postIdentifier) { (post) -> Void in
+                completion(success: true, post: post)
+            }
+        } else {
+            var post = post
+            post.save()
+            var comment = Comment(username: UserController.sharedController.currentUser!.username, text: text, postIdentifier: post.identifier!)
+            comment.save()
+            PostController.postFromIdentifier(comment.postIdentifier) { (post) -> Void in
+            completion(success: true, post: post)
+            }
+        }
     }
     
-    static func deleteComment(comment: Comment, completion: (Post?) -> Bool) {
+    static func deleteComment(comment: Comment, completion: (success: Bool, post: Post?) -> Void) {
+        comment.delete()
         
+        PostController.postFromIdentifier(comment.postIdentifier) { (post) -> Void in
+            completion(success: true, post: post)
+        }
     }
     
-    static func addLikeToPost(post: Post, completion: (Post?) -> Bool) {
-        
+    static func addLikeToPost(post: Post, completion: (success: Bool, post: Post?) -> Void) {
+        if let postIdentifier = post.identifier {
+            var like = Like(username: UserController.sharedController.currentUser!.username, postIdentifier: postIdentifier)
+            like.save()
+        } else {
+            var post = post
+            post.save()
+            var like = Like(username: UserController.sharedController.currentUser!.username, postIdentifier: post.identifier!)
+            like.save()
+        }
+        PostController.postFromIdentifier(post.identifier!, completion: { (post) -> Void in
+            completion(success: true, post: post)
+        })
     }
     
-    static func deleteLike(like: Like, completion: (Post?) -> Bool) {
-        
+    static func deleteLike(like: Like, completion: (success: Bool, post: Post?) -> Void) {
+        like.delete()
+        PostController.postFromIdentifier(like.postIdentifier) { (post) -> Void in
+        completion(success: true, post: post)
+        }
     }
     
     static func orderPosts(posts: [Post]) -> [Post] {
-        return []
+        return posts.sort({$0.0.identifier > $0.1.identifier})
     }
     
-    static func mockPosts() -> [Post] {
-        let imageString = "-K1l4125TYvKMc7rcp5e"
-        return [Post(imageEndPoint: imageString, caption: nil, username: "Me", identifier: nil)]
-    }
 }
