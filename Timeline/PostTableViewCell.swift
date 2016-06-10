dc0b5255c0b839995337cef37f4d9f92fbd1447b
Joshua Bates <batesjoshua50@gmail.com>
2016-06-10 01:37:23 -0600

    ViewControllers

---
+++ Timeline/PostTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostTableViewCell.swift
+//  Timeline
+//
+//  Created by Joshua Bates on 6/10/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    func updateWithPost(post: Post) {
+//        self.postImageView.image = nil
+//        self.likesLabel.text = "\(post.likes.count) likes"
+//        self.commentsLabel.text = "\(post.comments.count) comments"
+//        ImageController.imageForIdentifier(post.endpoint) { (image) -> Void in
+//            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+//                self.postImageView.image = image
+//            })
+//        }
+    }
+    
+}
